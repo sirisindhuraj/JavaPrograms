25c2753fcc4e3243891c954e735373377d94fa01
Siri Sindhu Raj <sirisindhuraj@Siris-MacBook-Air.local>
2024-11-04 10:54:11 +0530

    sum of numbers-2

--- untitled/src/addition.java
+++ untitled/src/addition.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class addition {
+    public static void main(String[] args) {
+        int a=7;
+        int b=8;
+        int c=a+b;
+        System.out.println("sum "+c);
+    }
 }
+
