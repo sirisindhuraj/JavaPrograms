2d0c1ccf9d63ec56f67fe3aff655eea959f94903
Siri Sindhu Raj <sirisindhuraj@gmail.com>
2025-10-28 21:21:02 +0530

    CodeForcese

---
+++ Interviewbit/IsASquare.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int h=0;h<t;h++){
+		    int a,b,c,d;
+		    a=sc.nextInt();
+		    b=sc.nextInt();
+		    c=sc.nextInt();
+		    d=sc.nextInt();
+		    if((a==b&&b==c)&&c==d){
+                System.out.println("Yes");
+		    }
+		    else{
+		        System.out.println("No");
+		    }
+		}
+	}
+}
